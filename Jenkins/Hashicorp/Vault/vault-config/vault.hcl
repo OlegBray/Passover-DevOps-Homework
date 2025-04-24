47e72d4dd9d0be84ed9845837d0fade9ae357222
Oleg, Braylovskiy <oleghoog123@gmail.com>
2025-04-24 10:31:38 +0300

    .

---
+++ Jenkins/Hashicorp/Vault/vault-config/vault.hcl
@@ -0,0 +1,13 @@
+storage "consul" {
+  address = "consul:8500"
+  path    = "vault/"
+}
+
+listener "tcp" {
+  address     = "0.0.0.0:8200"
+  tls_disable = true
+}
+
+disable_mlock = true
+
+ui = true
---
+++ Route53/files/update-route53.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DOMAIN_NAME="aws.cts.care."
+TAG_NAME="Name"
+SUB_DOMAIN_PREFIX="<Your Subdomain Prefix>"
+ROUTE53_MAIN_DOMAIN_ID=$(aws route53 list-hosted-zones | jq -r --arg domain "$DOMAIN_NAME" '.HostedZones[] | select(.Name==$domain) | .Id')
+FULL_DOMAIN="${SUB_DOMAIN_PREFIX}.${DOMAIN_NAME}"
+
+
+TOKEN=$(curl -s -X PUT -H "X-aws-ec2-metadata-token-ttl-seconds: 3600" http://169.254.169.254/latest/api/token)
+
+IP_PUBLIC=$(curl http://169.254.169.254/latest/meta-data/public-ipv4 -H "X-aws-ec2-metadata-token: $TOKEN")
+
+CURRENT_ROUTE53_IP=$(aws route53 list-resource-record-sets --hosted-zone-id "$ROUTE53_MAIN_DOMAIN_ID" \
+  | jq -r --arg name "$FULL_DOMAIN" '.ResourceRecordSets[] | select(.Name==$name) | .ResourceRecords[].Value')
+
+if [[ "$IP_PUBLIC" != "$CURRENT_ROUTE53_IP" && -n "$IP_PUBLIC" ]]; then
+  aws route53 change-resource-record-sets --hosted-zone-id "$ROUTE53_MAIN_DOMAIN_ID" --change-batch "$(
+    cat <<EOF
+{
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "${FULL_DOMAIN}",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          {
+            "Value": "$IP_PUBLIC"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+  )"
+else
+  echo "No update needed."
+fi
